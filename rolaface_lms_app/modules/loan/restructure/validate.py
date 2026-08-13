bd4d03b47e9fac9fd500fa1b42b606278b93831d
Abhishek Sati <abhishek.sati@rolaface.com>
2026-08-13 11:06:12 +0200

    removed debug

--- rolaface_lms_app/modules/loan/restructure/validate.py
+++ rolaface_lms_app/modules/loan/restructure/validate.py
@@ -22,7 +22,5 @@ def validate_update_payload(data):
 def validate_get_by_id(name):
     if not name.get("name"):
         frappe.throw("name is required")
-    print(repr(name))  # repr() will reveal hidden whitespace/type issues immediately
-    print(frappe.db.exists("Loan Restructure", name))
     if not frappe.db.exists("Loan Restructure", name.get("name")):
         frappe.throw(f"Loan Restructure '{name}' does not exist.")

b8b7c78724415b7d9db4718178c4da2d13aeaf96
Akhilesh Rawat <akhileshrawat.asr@gmail.com>
2026-08-24 16:28:26 +0200

    fix(repayment): error

--- rolaface_lms_app/modules/loan/repayment/api.py
+++ rolaface_lms_app/modules/loan/repayment/api.py
@@ -173,7 +173,7 @@ def delete():
 def update_status():
     repayment_id = frappe.local.form_dict.get("id")
     action = frappe.local.form_dict.get("action")
-    frappe.error_log(f"Repayment_ID --> {repayment_id}")
+    frappe.log_error(f"Repayment_ID --> {repayment_id}")
     try:
         result = service.update_loan_repayment_status(repayment_id, action)
         return send_response(

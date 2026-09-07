cc3134fac958f7a21fbc15d9075ce8d6e196136c
Akhilesh Rawat <akhileshrawat.asr@gmail.com>
2026-09-07 14:05:35 +0200

    fix(customer): 'NoneType' object is not iterable

--- rolaface_lms_app/modules/customer/service.py
+++ rolaface_lms_app/modules/customer/service.py
@@ -104,7 +104,7 @@ def get_customer_by_id(customer_id: str) -> Dict[str, Any]:
         raw_result[db_table_name] = [
             {field: value for field, value in row.as_dict().items() if field in allowed_fields}
             # for row in doc.get(db_table_name, [])
-            for row in (doc.get(db_table_name) or []):
+            for row in (doc.get(db_table_name) or [])
         ]
 
     raw_result["status"] = "active" if not doc.disabled else "inactive"
